447e28eace98ec6e4f0c1b93223aec0818026459
Yash4900 <satra.y@somaiya.edu>
2021-12-24 19:15:38 +0530

    modified update utility

--- update.py
+++ update.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///log.db'
@@ -21,12 +22,20 @@ inp = int(input('Enter record id: '))
 
 log = VehicleLog.query.get(inp)
 
-choice = int(input('Press 1 to update exit time and 2 to update entry time: '))
+choice = int(input('Press to update\n1. exit time\n2. entry time\n3. exit date\n4. entry date\n'))
 if choice == 1:
 	inp = input('Enter exit time: ')
 	log.exit_time = inp
-else: 
+elif choice == 2: 
 	inp = input('Enter entry time: ')
 	log.entry_time = inp
+elif choice == 3:
+	inp = int(input('Enter days before: '))
+	date = (datetime.datetime.today() - datetime.timedelta(days=inp)).date()
+	log.exit_date = date
+else:
+	inp = int(input('Enter days before: '))
+	date = (datetime.datetime.today() - datetime.timedelta(days=inp)).date()
+	log.entry_date = date
 
 db.session.commit()
